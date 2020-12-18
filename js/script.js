195a03e83fb60f17c830a1a50b698f6e3a9aab1f
Alessandro De Marco <lookatde.lad@gmail.com>
2020-12-18 10:16:45 +0100

    SNACK terminato

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// CICLO FOR
+
+// var ris = 0;
+// for (var i = 0; i < 5; i++) {
+//   var num = parseInt(prompt('Inserisci un numero'));
+//   ris = ris + num;
+// }
+// document.getElementById('risultato').innerHTML = ris;
+
+
+// DO WHILE
+
+// var ris = 0;
+// var i = 0;
+// do {
+//   var num = parseInt(prompt('Inserisci un numero'));
+//   ris = ris + num;
+//   i++;
+// } while (i < 5);
+// document.getElementById('risultato').innerHTML = ris;
